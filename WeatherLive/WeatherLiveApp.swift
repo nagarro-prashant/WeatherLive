cae32e12b13cfdfc460ddf0ea0000878884a562c
Prashant Gautam <prashant.gautam@nagarro.com>
2024-12-01 20:55:40 +0530

    Initial Commit

---
+++ WeatherLive/WeatherLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherLiveApp.swift
+//  WeatherLive
+//
+//  Created by Prashant Gautam on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
